6f44d056aa576d44665a2abdbde82621e200ddc0
jcahal <jacahal@gmail.com>
2016-02-05 13:44:37 -0700

    v1.1 Takes care of new lines

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int chunkSize = 10;
 int main(int argc, char* argv[]) {
 
     if(argc < 3) {
-        printf("Not enough args\n");
+        cout << "Not enough args" << endl;
     }
 
     ifstream inFile;
@@ -23,25 +23,30 @@ int main(int argc, char* argv[]) {
     string chunk;
 
     while(getline(inFile, line)){                                   // while line, put it in line
-        for(i = 0; i < line.size(); i++){                           // iterate over each char in line
-            chunk.push_back(line[i]);                               // put each char in chunk
 
-            if(i == (line.size() - 1) && (i % chunkSize) == 0)      // if EoL and chunk is filled
-            {
-                outFile.write(chunk.c_str(), chunk.size());         // dump chunk into outFile
-                chunk.clear();                                      // clear chunk
-                outFile.put('\n');                                  // print \n
-            }
-            else if(i == (line.size() - 1) && (i % chunkSize) != 0) // if EoL and chunk is NOT filled
-            {
-                outFile.write(chunk.c_str(), chunk.size());         // dump chunk into outFile
-                chunk.clear();                                      // clear chunk
-                outFile.put('\n');                                  // print \n
-            }
-            else if((i % chunkSize) == 0)                           // if chunk filled
-            {
-                outFile.write(chunk.c_str(), chunk.size());         // dump chunk into outFile
-                chunk.clear();                                      // clear chunk
+        if(line.size() == 0) {
+            outFile.put('\n');
+        } else {
+            for(i = 0; i < line.size(); i++){                           // iterate over each char in line
+                chunk.push_back(line[i]);                               // put each char in chunk
+
+                if(i == (line.size() - 1) && (i % chunkSize) == 0)      // if EoL and chunk is filled
+                {
+                    outFile.write(chunk.c_str(), chunk.size());         // dump chunk into outFile
+                    chunk.clear();                                      // clear chunk
+                    outFile.put('\n');                                  // print \n
+                }
+                else if(i == (line.size() - 1) && (i % chunkSize) != 0) // if EoL and chunk is NOT filled
+                {
+                    outFile.write(chunk.c_str(), chunk.size());         // dump chunk into outFile
+                    chunk.clear();                                      // clear chunk
+                    outFile.put('\n');                                  // print \n
+                }
+                else if((i % chunkSize) == 0)                           // if chunk filled
+                {
+                    outFile.write(chunk.c_str(), chunk.size());         // dump chunk into outFile
+                    chunk.clear();                                      // clear chunk
+                }
             }
         }
     }
